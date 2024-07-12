05b7e6ebb77c4698149c61669743795f1b84a624
Vtrier <168623374+Vtrier@users.noreply.github.com>
2024-07-12 17:31:16 -0300

    Add files via upload

---
+++ Javascript/Exemplo04 - Vetor/treino.js
@@ -0,0 +1,12 @@
+let nomes = [];
+let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"];
+let body = document.querySelector("body");
+
+function cadastrar() {
+    let tagInput = document.getElementById("nome");
+    let nomeCadastro = tagInput.value;
+    let tagSelectEstado = document.getElementById("estado");
+
+}
+
+
